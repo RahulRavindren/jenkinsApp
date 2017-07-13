3bd29c42122c25ec3907f8a5baf44da1e41e0a87
Rahul Ravindran <rahul.r945@gmail.com>
2017-07-13 23:45:46 +0530

    * java doc to utils * base utils initial commit

--- app/src/main/java/com/app/jenkinsapp/utils/BaseUtils.java
+++ app/src/main/java/com/app/jenkinsapp/utils/BaseUtils.java
@@ -11,9 +11,13 @@ public final class BaseUtils {
   private static final String EMPTY_STRING = "";
   private static volatile BaseUtils instance;
 
+  // private
   private BaseUtils() {
   }
 
+  /*
+  * single instance
+  */
   public static BaseUtils getInstance() {
     if (instance == null) {
       synchronized (BaseUtils.class) {
@@ -23,22 +27,45 @@ public final class BaseUtils {
     return instance;
   }
 
+  /**
+   * @param str - input string
+   * @return - boolean true/false
+   */
   public boolean isEmpty(final String str) {
     return str == null || str.trim().equals(EMPTY_STRING);
   }
 
+  /**
+   * Checks if collection is empty
+   *
+   * @param collection - input collection
+   * @return - boolean true/false
+   */
   public boolean isEmpty(final Collection collection) {
     return collection == null || collection.isEmpty();
   }
 
+  /**
+   * @param array - input object array
+   * @return - boolean true/false
+   * */
   public boolean isEmpty(final Object[] array) {
     return array == null || array.length == 0;
   }
 
+  /**
+   * @param map - input map
+   * @return boolean - true/false
+   * */
   public boolean isEmpty(final Map map) {
     return map == null || map.isEmpty();
   }
 
+  /**
+   * @param destinationMap - destination map object copyTo
+   * @param sourceMap - source map object copyFrom
+   * @return - copied Map object (destination)
+   * */
   public <T, V> Map<T, V> copyAll(final Map<T, V> destinationMap, final Map<T, V> sourceMap) {
     if (isEmpty(sourceMap)) {
       return destinationMap;
@@ -52,22 +79,49 @@ public final class BaseUtils {
     return destinationMap;
   }
 
+  /**
+   * equals objects
+   * @param a - object for compare
+   * @param b - object for compare
+   * @return - boolean true/false
+   * */
   public boolean equals(final Object a, final Object b) {
     return (a == null) ? (b == null) : (b != null && a.equals(b));
   }
 
+  /**
+   * compares string ignore case
+   * @param a - String for compare
+   * @param b - String for compare
+   * @return - boolean true/false
+   * */
   public boolean equalsIgnoreCase(final String a, final String b) {
     return (a == null) ? (b == null) : (b != null && a.equalsIgnoreCase(b));
   }
 
+  /**
+   * check string is valid number
+   * @param str - String input
+   * @return  - boolean true/false
+   * */
   public boolean isValidInteger(final String str) {
     return !isEmpty(str) && str.matches("[0-9]+");
   }
 
+  /**
+   * @param str - String input
+   * @return - boolean true/false
+   * */
   public boolean isEmptyWithoutTrim(final String str) {
     return !(str != null && (str.length() > 0));
   }
 
+  /**
+   * checks lists are equal
+   * @param firstList - first array
+   * @param secondList - second array
+   * @return - boolean true/false
+   * */
   public boolean equalsList(final List<?> firstList, final List<?> secondList) {
     if (isEmpty(firstList) || isEmpty(secondList)) {
       return false;
@@ -85,6 +139,11 @@ public final class BaseUtils {
     return true;
   }
 
+  /**
+   * checks if object available in weak-reference
+   * @param weakReference - weak reference of a type
+   * @return - boolean true/false
+   * */
   public <T> boolean isWeakReferenceObjectAvailable(final WeakReference<T> weakReference) {
     try {
       return weakReference != null && weakReference.get() != null;
---
+++ app/src/main/java/com/app/jenkinsapp/utils/Utils.java
@@ -0,0 +1,171 @@
+package com.app.jenkinsapp.utils;
+
+
+import android.app.Application;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
+
+public final class Utils {
+  private volatile Utils instance;
+  private Application application;
+  private Context context;
+
+  //private
+  private Utils() {
+  }
+
+  /*
+  * single instance
+  */
+  public Utils getInstance(final Application application) {
+    if (instance == null) {
+      synchronized (this) {
+        if (instance == null) {
+          instance = new Utils();
+          this.application = application;
+          this.context = application.getApplicationContext();
+        }
+      }
+    }
+    return instance;
+  }
+
+  /**
+   * convert pixel to DP
+   *
+   * @param pixel - pixel value
+   * @return - int DP
+   */
+  public int getDpFromPixels(final int pixel) {
+    final float scale = context.getResources().getDisplayMetrics().density;
+    return (int) (pixel / scale);
+  }
+
+  /**
+   * convert DP to pixel
+   *
+   * @param dp - DP value
+   * @return - int pixel
+   */
+  public int getPixelFromDp(final int dp) {
+    final float scale = context.getResources().getDisplayMetrics().density;
+    return (int) (dp * scale);
+  }
+
+  /**
+   * get device screen width
+   *
+   * @return - int
+   */
+  public int getDeviceScreenWidth() {
+    if (application == null || application.getResources() == null ||
+            application.getResources().getDisplayMetrics() == null) {
+      return 0;
+    }
+    return application.getResources().getDisplayMetrics().widthPixels;
+  }
+
+  /**
+   * get device screen height
+   *
+   * @return - int
+   */
+  public int getDeviceScreenHeight() {
+    if (application == null || application.getResources() == null ||
+            application.getResources().getDisplayMetrics() == null) {
+      return 0;
+    }
+    return application.getResources().getDisplayMetrics().heightPixels;
+  }
+
+  /**
+   * get device screen density
+   *
+   * @return - int
+   */
+  public int getDeviceDensity() {
+    if (application == null || application.getResources() == null ||
+            application.getResources().getDisplayMetrics() == null) {
+      return 0;
+    }
+    return (int) application.getResources().getDisplayMetrics().density;
+  }
+
+  /**
+   * get device screen width in DP
+   *
+   * @return - int
+   */
+  public int getDeviceScreenWidthDP() {
+    float density = getDeviceDensity();
+    if (density == 0) {
+      density = 1;
+    }
+    return (int) (getDeviceScreenWidth() / density);
+  }
+
+  /**
+   * get device screen height in DP
+   *
+   * @return - int
+   */
+  public int getDeviceScreenHeightDP() {
+    float density = getDeviceDensity();
+    if (density == 0) {
+      density = 1;
+    }
+    return (int) (getDeviceScreenHeight() / density);
+  }
+
+  /**
+   * Drawable from resId
+   *
+   * @param resId - int drawable resId
+   * @return - Drawable
+   */
+  public Drawable getDrawable(@DrawableRes final int resId) {
+    if (Build.VERSION.SDK_INT < 21) {
+      return context.getResources().getDrawable(resId);
+    } else {
+      return context.getDrawable(resId);
+    }
+  }
+
+  /**
+   * get string from resources
+   *
+   * @param args  - args
+   * @param resId - int string resId
+   * @return - String
+   */
+  public String getString(@StringRes final int resId, final Object... args) {
+    return application.getString(resId, args);
+  }
+
+  /**
+   * @param resId    - int string resId
+   * @param args     - args
+   * @param quantity - quantity field
+   * @return - String
+   */
+  public String getQuantifiedString(final int resId,
+                                    final int quantity,
+                                    final Object... args) {
+    return application.getResources().getQuantityString(resId, quantity, args);
+  }
+
+  /**
+   * get string array from resources
+   *
+   * @param resId - int string-array resId
+   * @return - String array
+   */
+  public String[] getStringArray(final int resId) {
+    return application.getResources().getStringArray(resId);
+  }
+
+
+}
