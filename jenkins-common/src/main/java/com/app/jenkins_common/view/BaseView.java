15672efac5a86fecca7a0048b0fd784ba4f710bf
Rahul Ravindran <rahul.r945@gmail.com>
2017-07-25 21:40:59 +0530

    initial commit view and model

--- jenkins-common/src/main/java/com/app/jenkins_common/view/BaseView.java
+++ jenkins-common/src/main/java/com/app/jenkins_common/view/BaseView.java
@@ -1,5 +1,10 @@
 package com.app.jenkins_common.view;
 
 
-public interface BaseView {
+import com.app.jenkins_common.model.DataModel;
+
+public interface BaseView<M extends DataModel> {
+  int getViewId();
+
+  void updateView(M model);
 }
