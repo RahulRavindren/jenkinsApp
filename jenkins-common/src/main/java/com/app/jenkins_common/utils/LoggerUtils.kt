96ff6316c92a9c7763d51d81495beef2bc3dba1e
Rahul Ravindran <rahul.r945@gmail.com>
2017-09-07 23:15:03 +0530

    gradle version upgraded and logger utils added

--- jenkins-common/src/main/java/com/app/jenkins_common/utils/LoggerUtils.kt
+++ jenkins-common/src/main/java/com/app/jenkins_common/utils/LoggerUtils.kt
@@ -1,6 +1,42 @@
 package com.app.jenkins_common.utils
 
+import timber.log.Timber
+
 class LoggerUtils {
-    companion object {
+
+    fun debug(key: String, message: String) {
+        Timber.d(key + " : " + message)
+    }
+
+    fun debug(message: String, vararg args: Object) {
+        Timber.d(message, args)
+    }
+
+    fun debug(throwable: Throwable) {
+        Timber.d(throwable);
+    }
+
+    fun error(key: String, message: String) {
+        Timber.d(key + " : " + message)
+    }
+
+    fun error(message: String, vararg args: Object) {
+        Timber.d(message, args)
+    }
+
+    fun error(throwable: Throwable) {
+        Timber.d(throwable);
+    }
+
+    fun info(key: String, message: String) {
+        Timber.d(key + " : " + message)
+    }
+
+    fun info(message: String, vararg args: Object) {
+        Timber.d(message, args)
+    }
+
+    fun info(throwable: Throwable) {
+        Timber.d(throwable);
     }
 }
